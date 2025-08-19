1f0b23d6ab9c6f9ad9ddc5ac55fd0b2e09e3d7d7
YashPratap17 <yashpsingh17@gmail.com>
2025-08-19 16:16:01 +0530

    New Commit

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from blog.models import Posts
 
 def blog_index(request):
     return render(request=request, template_name='blog_index.html')
 def list_posts(request):
-    return render(request=request, template_name='posts.html')    
+    return render(request=request, template_name='blog/posts.html')
--- myproject/settings.py
+++ myproject/settings.py
@@ -51,11 +51,12 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'myproject.urls'
+
 TEMPLATES_DIRS = BASE_DIR / 'myproject' / 'templates'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIRS],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
